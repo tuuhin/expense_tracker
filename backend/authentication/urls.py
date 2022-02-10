f6a6771bba6c0e78fbdda421fe843edb4ccec9b4
Tuhin <bhowmicktuhhin34@gmail.com>
2022-02-11 00:31:46 +0530

    whitenoise for serving static files

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -4,6 +4,6 @@ from .views import register_users, change_password
 urlpatterns = [
     path('create', register_users, name="register_user"),
     path('token', TokenObtainPairView.as_view(), name='get_token'),
-    path('refresh_token', TokenRefreshView.as_view(), name='refresh_token'),
+    path('refresh', TokenRefreshView.as_view(), name='refresh_token'),
     path('change_password', change_password, name="change_password"),
 ]
--- backend/expense_tracker/settings.py
+++ backend/expense_tracker/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -175,6 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR,'static/root')
 STATICFILES_DIRS = (path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
