bdfb010c6037e7333361d27d631c264d648853b3
tuuhin <tuhinbhowmick2513@gmail.com>
2022-03-17 20:38:33 +0530

    thinking about  some new pages

---
+++ frontend/lib/app/home/routes/add_reminder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddReminder extends StatelessWidget {
+  const AddReminder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [TextField(), TextField(), TextField()],
+        ),
+      ),
+    );
+  }
+}
